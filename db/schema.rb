8efd966cd9daa91efbab256f2f0693e96ba0d14f
dewanhimanshu <himanshudewan786@gmail.com>
2019-01-12 22:05:26 +0530

    Posts made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_152814) do
+ActiveRecord::Schema.define(version: 2019_01_12_162619) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
