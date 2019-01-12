19d9ec4d704cef8dfb1a1b031dad89dfee74af6a
dewanhimanshu <himanshudewan786@gmail.com>
2019-01-12 23:24:18 +0530

    admin ui added

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+ENV['RAILS_ADMIN_THEME'] = 'rollincode'
+
+
 module WasteMiner
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
